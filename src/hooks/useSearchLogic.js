c0bb1e195768fde308bb42d0e80d6916e574f107
John Hillert <john.hillert@gmail.com>
2021-05-24 12:48:15 -0500

    search logic working

--- src/hooks/useSearchLogic.js
+++ src/hooks/useSearchLogic.js
@@ -1,19 +1,29 @@
 import { useEffect, useRef } from 'react';
+import cloneDeep from 'lodash/cloneDeep';
+import * as data from '../test-data';
 
-const containsExactMatch = records => {
-
+const getRecordsWithExactMatch = (records, str) => {
+    return cloneDeep(
+        records.filter(record => Object.values(record).some(value => String(value).includes(str)))
+    );
 }
 
 function useSearchLogic(searchQuery, records, setRecordsCallback) {
+    let newRecords = [];
     const ref = useRef();
 
     useEffect(() => {
-        console.log(searchQuery);
+        newRecords = getRecordsWithExactMatch(records, searchQuery);
+        setRecordsCallback(newRecords);
     }, [searchQuery]);
 
     useEffect(() => {
-        console.log(records);
+        // console.log(records);
     }, [records]);
 }
 
 export default useSearchLogic;
+
+const testData = data.smallUserData;
+const testStr = '9521';
+console.log(getRecordsWithExactMatch(testData, testStr));
