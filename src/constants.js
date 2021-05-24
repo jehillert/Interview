47ffba1c268fca2c0ecb0584824ea1854094134a
John Hillert <john.hillert@gmail.com>
2021-05-24 02:19:09 -0500

    renamed tabled field for surveys

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,32 @@
-export const defaultTableFields = ['FirstName', 'LastName', 'Review', 'Rating'];
+export const surveyTableFields = ['FirstName', 'LastName', 'Review', 'Rating'];
+export const userTableFields = ['firstName', 'lastName', 'username', 'password'];
+
+
+/*
+USER FIELDS:
+  'birthdate'
+  'email'
+  'firstName'
+  'gender'
+  'lastName'
+  'location': {
+      'city'
+      'postcode'
+      'state'
+      'street'
+  },
+  'password'
+  'phone'
+  'picture'
+  'title'
+  'username'
+
+SURVEY FIELDS:
+  'ID',
+  'Review',
+  'Rating',
+  'ProviderEmail',
+  'FirstName',
+  'LastName',
+  'ReviewCompletedTimeStamp',
+ */
